6195af6408b2df7675684633bdc26b61cb0a4799
benny <benny08118@gmail.com>
2021-01-05 23:26:35 +0800

    ac and del  feature done

--- under3F/under3f.js
+++ under3F/under3f.js
@@ -1,5 +1,34 @@
 
-var block=document.querySelector(".contant");
+var block=document.querySelector("p");
+var bodyclick=document.querySelectorAll(".number");
+let showProcess=document.querySelector(".process");
+const acDe=document.querySelectorAll(".acDe");
+for(i=0;i<bodyclick.length;i++){
+      bodyclick[i].addEventListener("click",function(){
+                  showValue(this.innerHTML);
+      });
+}
+
+
+for(i=0;i<acDe.length;i++){
+      acDe[i].addEventListener("click",function(){ 
+            if(this.innerHTML=='AC'){
+                  showProcess.innerHTML='';
+                  console.log('ac');
+                  console.log(this);
+            }else{
+                  s=showProcess.innerHTML
+                  showProcess.innerHTML=s.substr(0,s.length-1) ;
+                  console.log(s);
+            }
+      });
+}
+
+function showValue(keyIn){
+      
+      showProcess.innerHTML=showProcess.innerHTML+keyIn;
+}
+
 
 
 
