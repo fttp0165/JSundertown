c40c84888e98b4478456c6cb6546f73db423d3e6
benny <benny08118@gmail.com>
2021-01-05 23:42:11 +0800

    fix overflow

--- under3F/under3f.js
+++ under3F/under3f.js
@@ -3,29 +3,30 @@ var block=document.querySelector("p");
 var bodyclick=document.querySelectorAll(".number");
 let showProcess=document.querySelector(".process");
 const acDe=document.querySelectorAll(".acDe");
+const number=[];
 for(i=0;i<bodyclick.length;i++){
+      //偵測數字鍵並顯示至處理列
       bodyclick[i].addEventListener("click",function(){
                   showValue(this.innerHTML);
       });
 }
-
-
 for(i=0;i<acDe.length;i++){
       acDe[i].addEventListener("click",function(){ 
             if(this.innerHTML=='AC'){
+                  //讀取到‘ＡＣ’刪除處理列
                   showProcess.innerHTML='';
                   console.log('ac');
                   console.log(this);
             }else{
                   s=showProcess.innerHTML
+                  // substr(0,s.length-1)擷取字串第０個到倒數第二個
                   showProcess.innerHTML=s.substr(0,s.length-1) ;
-                  console.log(s);
             }
       });
 }
 
 function showValue(keyIn){
-      
+      // 將輸入顯示到處理列
       showProcess.innerHTML=showProcess.innerHTML+keyIn;
 }
 
